8a7e1ba6bb0292ee545d79a2e3b8dd7bbb39227b
Tirth Bhalani <tirthbhalani12345@gmail.com>
2025-09-29 12:10:30 +0530

    metadata and generateMetadata

--- routing/src/app/about/page.tsx
+++ routing/src/app/about/page.tsx
@@ -1,3 +1,8 @@
+export const metadata = {
+    title: "This is the about me page.",
+    description: "The owner of this page is Tirth "
+}
+
 export default function About(){
     return <h1>About me</h1>
 }
--- routing/src/app/products/[productId]/page.tsx
+++ routing/src/app/products/[productId]/page.tsx
@@ -1,5 +1,18 @@
+
+import { Metadata } from "next";
+type Props  = {
+    params : Promise<{productId : string}>
+}
+
+export const generateMetadata = async ({params} : Props): Promise<Metadata> =>{
+    const id = (await params).productId
+    return {
+        title : `Product ${id}`
+    }
+}
+
 export default async function productDetails(
-    {params} : {params : Promise<{productId : string}>}
+    {params} : Props
 ){
     const pid = (await params)?.productId ?? "No pid";
     console.log("Tis is pid : " + pid)
